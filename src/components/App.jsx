9aa1c04caf70a4799a953f9a308445bb4f0ec6fe
Kevin Smith <futuretense@gmail.com>
2021-03-02 12:48:28 -0800

    Completed challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMousedOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+    //console.log("clicked");
+  }
+
+  function overColor() {
+    setMouseOver(true);
+  }
+
+  function outColor() {
+    setMouseOver(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={overColor}
+        onMouseOut={outColor}
+      >
+        Submit
+      </button>
     </div>
   );
 }
 
 export default App;
+
+// style="background-color:black;"
+//onMouseOver={{ backgroundColor: "black" }}
